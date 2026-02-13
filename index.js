29961e991f09ff6b7d4b063a0016fe434e05c1ac
NikoDivinex <romannajibi1@gmail.com>
2026-02-13 17:24:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ function getClientIP(req) {
 const server = http.createServer(async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') { res.writeHead(204); res.end(); return; }
 
